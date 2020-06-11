36497af595a3b126165ecd65a3eb84cf8342dd38
shin-ga-eun <sge7102@naver.com>
2020-06-11 18:27:15 +0900

    member entity 추가.

---
+++ app/src/main/java/com/bugkillers/sea/domain/entity/Member.java
@@ -0,0 +1,41 @@
+package com.bugkillers.sea.domain.entity;
+
+
+public class Member {
+    private Long id;
+    private String email;
+    private String password;
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
