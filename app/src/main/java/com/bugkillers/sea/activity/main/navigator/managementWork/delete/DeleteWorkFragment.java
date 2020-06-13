48ad3d3f368381d05d2aaaa21997bc5b2c464fda
shin-ga-eun <sge7102@naver.com>
2020-06-14 01:37:25 +0900

    #10 작품리스트에서 수정,삭제 페이지 이동 및 팝업 UI 구현.
    navigate fragment 화면 이동

--- app/src/main/java/com/bugkillers/sea/activity/main/navigator/managementWork/delete/DeleteWorkFragment.java
+++ app/src/main/java/com/bugkillers/sea/activity/main/navigator/managementWork/delete/DeleteWorkFragment.java
@@ -1,28 +1,22 @@
-package com.bugkillers.sea.activity.main.navigator.listWork;
+package com.bugkillers.sea.activity.main.navigator.managementWork.delete;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.bugkillers.sea.R;
 
-public class ListWorkFragment extends Fragment {
-
+public class DeleteWorkFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        final View root = inflater.inflate(R.layout.fragment_listwork, container, false);
-
-
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_deletework, container, false);
 
         return root;
     }
 }
+
